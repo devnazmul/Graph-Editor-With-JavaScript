2af0e4ce50846fa18be1193b3efa0c64e8e61ff4
Md Nazmul Islam <md.nazmul.islam1332@gmail.com>
2023-10-22 09:39:49 +0600

    Create utils.js

---
+++ js/math/utils.js
@@ -0,0 +1,16 @@
+const getNearestPoint = (loc, points, threshold = 15) => {
+  let minDist = Number.MAX_SAFE_INTEGER;
+  let nearest = null;
+  for (const point of points) {
+    const dist = distance(point, loc);
+    if (dist < minDist && dist < threshold) {
+      minDist = dist;
+      nearest = point;
+    }
+  }
+  return nearest;
+};
+
+const distance = (p1, p2) => {
+  return Math.hypot(p1.x - p2.x, p1.y - p2.y);
+};
